166d32df60bb9e023de99cb919cab53abaf52db3
anusha <anushakizhakkeveedu@gmail.com>
2023-11-16 09:50:36 +0530

    Add logging module and its features

--- create_vcf.py
+++ create_vcf.py
@@ -1,35 +1,121 @@
-import os 
+import os
+import requests
+import csv
+import sys
+import logging
+import argparse
 
-import csv 
+def parse_args():
+    parser = argparse.ArgumentParser(prog="create_vcf.py",
+                                     description="Generates sample employee database as csv")
+    parser.add_argument("input_file", help="Name of input csv file")
+    parser.add_argument("output_dir", help="Output directory for vCards and QR codes")
+    parser.add_argument("-v", "--verbose", help="Print detailed logging", action='store_true', default=False)
+    parser.add_argument("--add_qr", help="Add QR codes", action='store_true', default=False)
+    parser.add_argument("--qr_size", help="Size of QR code", type=int, default=500)
+    parser.add_argument("--address", help="Address for vCards")
+    args = parser.parse_args()
+    return args
 
-filename = "names.csv"
+def setup_logging(log_level):
+    global logger
+    logger = logging.getLogger("vcf_log")
+    logger.setLevel(logging.DEBUG)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(log_level)
+    console_formatter = logging.Formatter("[%(levelname)s] %(asctime)s | %(filename)s:%(lineno)d | %(message)s")
+    console_handler.setFormatter(console_formatter)
+    logger.addHandler(console_handler)
+
+    file_handler = logging.FileHandler("run.log")
+    file_handler.setLevel(logging.DEBUG)
+    file_formatter = logging.Formatter("[%(levelname)s] %(asctime)s | %(filename)s:%(lineno)d | %(message)s")
+    file_handler.setFormatter(file_formatter)
+    logger.addHandler(file_handler)
 
 def create_vcard(file):
     last_name, first_name, title, email, phone = file
     content = f"""BEGIN:VCARD
-    VERSION:2.1
-    N:{last_name};{first_name}
-    FN:{first_name} {last_name}
-    ORG:Authors, Inc.
-    TITLE:{title}
-    TEL;WORK;VOICE:{phone}
-    ADR;WORK:;;100 Flat Grape Dr.;Fresno;CA;95555;United States of America
-    EMAIL;PREF;INTERNET:{email}
-    REV:20150922T195243Z
-    END:VCARD
-    """
+VERSION:2.1
+N:{last_name};{first_name}
+FN:{first_name} {last_name}
+ORG:Authors, Inc.
+TITLE:{title}
+TEL;WORK;VOICE:{phone}
+ADR;WORK:;;100 Flat Grape Dr.;Fresno;CA;95555;United States of America
+EMAIL;PREF;INTERNET:{email}
+REV:20150922T195243Z
+END:VCARD
+"""
     return content
 
 def read_csv(file_name):
     data = []
-    with open(file_name,"r") as file:
-        csv_file = csv.reader(file)
-        for row in csv_file:
-            data.append(row)
+    if os.path.isfile(file_name):
+        with open(file_name, "r") as file:
+            csv_file = csv.reader(file)
+            data = [row for row in csv_file]
+    else:
+        logger.error(f"File not found: {file_name}")
+        sys.exit(1)
     return data
 
-def parse_csv(file_name):
+def parse_csv(file_name, args): 
     data = read_csv(file_name)
     for row in data:
         vcard_content = create_vcard(row)
+        create_vcard_file(row, vcard_content, args)  
+        if args.add_qr:
+            generate_qr_code(row, vcard_content, args)
+
+
+counter = 0
+
+def create_vcard_file(row_data, content, args):
+    global counter  
+
+    file_path = os.path.join(args.output_dir, f"{row_data[1].lower()}_{row_data[0].lower()}.vcf")
+    
+    if os.path.exists(file_path):
+        logger.warning(f"File already exists: {file_path}")
+    else:
+        with open(file_path, "w") as file:
+            file.write(content)
+            counter += 1 
+            logger.info(f"Created vCard ({counter}): {file_path}")
+
         
+
+def generate_qr_code(row_data, content, args):
+    qr_url = requests.get(
+        f"https://chart.googleapis.com/chart?cht=qr&chs={args.qr_size}x{args.qr_size}&chl={content}"
+    )
+    file_path = f"{args.output_dir}/{row_data[1].lower()}_{row_data[0].lower()}.qr.png"
+    
+    if os.path.exists(file_path):
+        logger.warning(f"File already exists: {file_path}")
+    else:
+        
+        if os.access(args.output_dir, os.W_OK):
+            with open(file_path, "wb") as file:
+                file.write(qr_url.content)
+        else:
+            f"No write access to directory"
+
+
+
+def main():
+    args = parse_args()
+    if args.verbose:
+        setup_logging(logging.DEBUG)
+    else:
+        setup_logging(logging.INFO)
+
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
+
+    parse_csv(args.input_file, args)
+
+if __name__ == "__main__":
+    main()
