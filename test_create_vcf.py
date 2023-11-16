03e5567d2a06ba15635530d07f4b12317273af43
anusha <anushakizhakkeveedu@gmail.com>
2023-11-16 22:41:47 +0530

    modified the test for clear_output_dir

--- test_create_vcf.py
+++ test_create_vcf.py
@@ -1,9 +1,8 @@
-from create_vcf import create_vcard, read_csv, parse_csv
+from create_vcf import create_vcard, read_csv,parse_csv
 import csv
-import os 
+import os
+import argparse
 
-
-file = "names.csv"
 
 def test_create_vcard():
     lname = "Mason"
@@ -29,23 +28,51 @@ END:VCARD
     content = create_vcard(file)
     assert not content == expected_content
 
-def test_read():
-    data = [['Mason', 'Nicole', 'Buyer, retail', 'nicol.mason@gibson.com', '(871)967-6024x82190'], 
-                           ['Walker', 'Steve', 'Accommodation manager', 'steve.walke@hicks.info', '(876)953-8282x713']]
 
-    with open('news.csv', 'w',newline='') as csvfile:
+def test_read():
+    data = [
+        [
+            "Mason",
+            "Nicole",
+            "Buyer, retail",
+            "nicol.mason@gibson.com",
+            "(871)967-6024x82190",
+        ],
+        [
+            "Walker",
+            "Steve",
+            "Accommodation manager",
+            "steve.walke@hicks.info",
+            "(876)953-8282x713",
+        ],
+    ]
+
+    with open("news.csv", "w", newline="") as csvfile:
         w = csv.writer(csvfile)
         for row in data:
             w.writerow(row)
 
-    result = read_csv('news.csv')
-    expected_result = [['Mason', 'Nicole', 'Buyer, retail', 'nicol.mason@gibson.com', '(871)967-6024x82190'], 
-                        ['Walker', 'Steve', 'Accommodation manager', 'steve.walke@hicks.info', '(876)953-8282x713']]
+    result = read_csv("news.csv")
+    expected_result = [
+        [
+            "Mason",
+            "Nicole",
+            "Buyer, retail",
+            "nicol.mason@gibson.com",
+            "(871)967-6024x82190",
+        ],
+        [
+            "Walker",
+            "Steve",
+            "Accommodation manager",
+            "steve.walke@hicks.info",
+            "(876)953-8282x713",
+        ],
+    ]
 
     assert result == expected_result
 
-def test_parse_csv():
-    parse_csv("names.csv")
+output_dir = "vcard"
+def test_clear_output_dir():
+    assert os.listdir(output_dir)
 
-    assert os.path.exists("vcards")
-    
